a329057c6e57f1a6bb1f1dfc49bfc1287c768f4e
raiizoor <mateus.informatica44@gmail.com>
2021-03-25 22:05:46 -0300

    Finishing section 5

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
+
+
+function CartScreen({ match, location, history }) {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
+    return (
+        <Row>
+            <Col md={8}>
+                <h1>Shooping Card</h1>
+                {cartItems.length === 0 ? (
+                    <Message variant='info'>
+                        Your carti is empty <Link to='/'>Go back</Link>
+                    </Message>
+                ) : (
+                    <ListGroup variant='flush'>
+                        {cartItems.map(item => (
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded />
+                                    </Col>
+                                    <Col md={3}>
+                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                    </Col>
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+                                </Row>                                
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
+            </Col>
+
+            <Col md={8}>
+            </Col>
+        </Row>
+    )
+}
+
+export default CartScreen
