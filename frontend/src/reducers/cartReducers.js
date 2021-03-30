90cca57d71a443ec82bd167943d013dcba4ca156
raiizoor <mateus.informatica44@gmail.com>
2021-03-30 14:34:07 -0300

    Finished Section 8: Checkout Process (Part 1)

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,6 +4,7 @@ import {
     CART_SAVE_SHIPPING_ADDRESS,
 
     CART_SAVE_PAYMENT_METHOD,
+    CART_CLEAR_ITEMS,
 } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
@@ -43,6 +44,12 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
             return {
                 ...state,
                 paymentMethod: action.payload
+            }
+
+        case CART_CLEAR_ITEMS:
+            return{
+                ...state,
+                cartItems:[]
             }
 
         default:
